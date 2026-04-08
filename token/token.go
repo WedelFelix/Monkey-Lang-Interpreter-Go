1c9f048de42d5e6b8da00b8582788e028fc75335
Felix Wedel <wedelfelix@hotmail.ca>
2026-04-07 19:15:36 -0500

    Add keywords to our tokens
    Add a map of keywords and a function that lets us check a string to see
    if it should be treated as a keyword or an identifier

--- token/token.go
+++ token/token.go
@@ -20,7 +20,7 @@ const (
 	PLUS   = "+"
 
 	//Delimiters
-	COMA      = ","
+	COMMA     = ","
 	SEMICOLON = ";"
 
 	LPAREN = "("
@@ -32,3 +32,15 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
